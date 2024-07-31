f7747be42d09013ff425a990e9cd130672def6ac
zaadevofc <zaadevofc@gmail.com>
2024-07-31 22:08:13 +0700

    [UPDATE] add tailwindcss

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Home | Zadocs Automitation';
+export const SITE_DESCRIPTION = 'Buat dokumenmu secara dinamis dan otomatis tanpa perlu ribet!!';
